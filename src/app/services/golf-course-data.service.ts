95cc634903e729150888054e6f8abca222ea9bf1
Zach G <zabros20@gmail.com>
2019-08-30 09:37:06 -0600

    changed the xml request to https

--- src/app/services/golf-course-data.service.ts
+++ src/app/services/golf-course-data.service.ts
@@ -14,7 +14,7 @@ export class GolfCourseDataService {
   constructor(private httpClient: HttpClient) { }
 
   getGolfCourses(): Observable<GolfCourse[]> {
-    const url = 'http://golf-courses-api.herokuapp.com/courses';
+    const url = 'https://golf-courses-api.herokuapp.com/courses';
     return this.httpClient.get<any>(url).pipe(map(data => data.courses));
   }
 }
