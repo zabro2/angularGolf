523a3e5ebc7feef915130b4976fc87b66edbf98c
Zach G <zabros20@gmail.com>
2019-08-30 08:04:30 -0600

    Commit 13, added to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXPZoFC_awGeBUS1maSuBlW8Js6gq4QS8",
+    authDomain: "angular-golf-37f3c.firebaseapp.com",
+    databaseURL: "https://angular-golf-37f3c.firebaseio.com",
+    projectId: "angular-golf-37f3c",
+    storageBucket: "",
+    messagingSenderId: "204396862096",
+    appId: "1:204396862096:web:29fc84554fa11cee"
+  }
 };
